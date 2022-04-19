bc6af148a935c6e7165ddd986c08e5de1c461bc3
williamwittig2 <wwittig@student.greenriver.edu>
2022-04-19 13:11:54 -0400

    Added index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once('vendor/autoload.php');
+
+// Create instance of the base class
+$f3 = Base::instance();
+
+// Define a default route
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+// Run fat free
+$f3->run();
