87114a5faa1b57c6f3f8c837f5d75edaf6570cb2
williamwittig2 <wwittig@student.greenriver.edu>
2022-06-02 17:21:24 -0400

    Added comments to the model classes

--- model/data-layer.php
+++ model/data-layer.php
@@ -1,22 +1,54 @@
 <?php
-class DataLayer {
-	// Get gender options
+
+/**
+ * This class is used to hold data to be displayed on the application
+ * @author William Wittig
+ * @version 1.0
+ */
+class DataLayer
+{
+	private $_dbh;
+
+	/**
+	 * This method consructs a data-layer object
+	 */
+	function __construct()
+	{
+		// TODO: Move try-catch from config to data-layer
+		require_once($_SERVER['DOCUMENT_ROOT'].'/../config.php');
+		$dbh = 5;
+		$this->_dbh = $dbh;
+	}
+
+	/**
+	 * This method returns an array of genders
+	 * @return string[]
+	 */
 	static function getGenders() {
 		return array("Male", "Female", "Non-Binary");
 	}
 
-	// Get indoor interests
+	/**
+	 * This method returns an array of indoor interests
+	 * @return string[]
+	 */
 	static function getIndoorInterests() {
 		return array("TV", "Movies", "Cooking", "Board Games", "Puzzles", "Reading",
 			"Playing Cards","Video Games");
 	}
 
-	// Get outdoor interests
+	/**
+	 * This method returns an array of outdoor interests
+	 * @return string[]
+	 */
 	static function getOutdoorInterests() {
 		return array("Hiking", "Biking", "Swimming", "Collecting", "Walking", "Climbing");
 	}
 
-	// Get states
+	/**
+	 * This method returns an array of states
+	 * @return string[]
+	 */
 	static function getStates() {
 		return array("Alabama","Alaska","Arizona","Arkansas","California","Colorado",
 			"Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois",
--- model/validation.php
+++ model/validation.php
@@ -1,8 +1,18 @@
 <?php
 
+/**
+ * This class is used validate data input from the user
+ * @author William Wittig
+ * @version 1.0
+ */
 class Validation {
 	// ----- VALIDATION FUNCTIONS -----
-	// Name validation
+
+	/**
+	 * This method validates that a valid name was input
+	 * @param $str
+	 * @return bool
+	 */
 	static function validName($str) {
 		for ($i=0;$i<strlen($str);$i++) {
 			// If any character is not alphabetic
@@ -15,13 +25,21 @@ class Validation {
 		return strlen(trim($str))>0;
 	}
 
-	// Age validation
+	/**
+	 * This method validates that a valid age was input
+	 * @param $age
+	 * @return bool
+	 */
 	static function validAge($age) {
 		// Checking if age is between 18 and 118
 		return ($age>=18 && $age<=118);
 	}
 
-	// Phone number validation
+	/**
+	 * This method validates that a valid phone number was input
+	 * @param $phone
+	 * @return bool
+	 */
 	static function validPhone($phone) {
 		for ($i=0;$i<strlen($phone);$i++) {
 			// Checking if any character is not a digit
@@ -34,13 +52,21 @@ class Validation {
 		return (strlen(trim($phone))==10 || strlen(trim($phone))==7);
 	}
 
-	// Email validation
+	/**
+	 * This method validates that a valid email was input
+	 * @param $email
+	 * @return mixed
+	 */
 	static function validEmail($email) {
 		// Ensuring the email has a '@' and '.'
 		return filter_var($email,FILTER_VALIDATE_EMAIL);
 	}
 
-	// Outdoor interests validation
+	/**
+	 * This method validates that valid outdoor interests were input
+	 * @param $outdoorInterests
+	 * @return bool
+	 */
 	static function validOutdoor($outdoorInterests) {
 		// If no outdoor interests were selected
 		if (empty($outdoorInterests)) {
@@ -59,7 +85,11 @@ class Validation {
 		return TRUE;
 	}
 
-	// Indoor interests validation
+	/**
+	 * This method validates that valid indoor interests were input
+	 * @param $indoorInterests
+	 * @return bool
+	 */
 	static function validIndoor($indoorInterests) {
 		// If no indoor interests were selected
 		if (empty($indoorInterests)) {
