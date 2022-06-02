a77d72fb6b18a01f5a10823159caccea06e6ff42
williamwittig2 <wwittig@student.greenriver.edu>
2022-06-02 17:20:57 -0400

    Added comments to index file

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * This class is calls the required pages through the controller
+ * @author William Wittig
+ * @version 1.0
+ */
+
 // Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -9,6 +15,9 @@ require_once('vendor/autoload.php');
 // Start the session
 session_start();
 
+// Test DataLayer class
+$dataLayer = new DataLayer();
+
 // Create instance of the base class
 $f3 = Base::instance();
 
@@ -17,31 +26,36 @@ $con = new Controller($f3);
 
 // Define a default route
 // Home page rendering
-$f3->route('GET /', function() {
+$f3->route('GET /', function()
+{
 	// Displaying the page
     global $con;
 	$con->home();
 });
 
-$f3->route('GET|POST /personalInfo', function() {
+$f3->route('GET|POST /personalInfo', function()
+{
 	// Displaying the page
 	global $con;
     $con->personalInfo();
 });
 
-$f3->route('GET|POST /profile', function() {
+$f3->route('GET|POST /profile', function()
+{
 	// Displaying the page
 	global $con;
 	$con->profile();
 });
 
-$f3->route('GET|POST /interests', function($f3) {
+$f3->route('GET|POST /interests', function($f3)
+{
 	// Displaying the page
 	global $con;
 	$con->interests();
 });
 
-$f3->route('GET|POST /profileSummary', function() {
+$f3->route('GET|POST /profileSummary', function()
+{
 	global $con;
 	$con->profileSummary();
 });
